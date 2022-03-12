bd3f36f1489fe2497816d86f41e26366e2865239
JoseLike <joseluisgil94@gmail.com>
2022-03-12 11:01:09 +0000

    subida de ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const rannumber = getinput => {
+  let random = Math.floor(Math.random() * getinput.length);
+  return random;
+};
+
+const excuses = () => {
+  let ran1 = who[rannumber(who)];
+  let ran2 = action[rannumber(action)];
+  let ran3 = what[rannumber(what)];
+  let ran4 = when[rannumber(when)];
+  return ran1 + " " + ran2 + " " + ran3 + " " + ran4;
+};
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuses();
 };
